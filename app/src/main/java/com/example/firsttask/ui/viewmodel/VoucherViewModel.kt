ab64252aa3de5bd1e013435b62949869783f4f8a
khoi-m1 <hain3628@gmail.com>
2024-08-19 09:12:58 +0700

    next

--- app/src/main/java/com/example/firsttask/ui/viewmodel/VoucherViewModel.kt
+++ app/src/main/java/com/example/firsttask/ui/viewmodel/VoucherViewModel.kt
@@ -94,14 +94,7 @@ class VoucherViewModel : ViewModel() {
         dataItem.data.forEach {
             if (it.id == itemVoucherState.id) {
                 if (it.status == ItemVoucherState.Status.Default) {
-
-                    val newDataTest = ItemVoucherState(
-                        id = "0",
-                        name = "\$10 Min of spend \$80 second line",
-                        provider = "Plaza Prenium",
-                        status = ItemVoucherState.Status.Default,
-                        amount = 10.0
-                    ).copy(status = ItemVoucherState.Status.Selected)
+                    val newDataTest = it.copy(status = ItemVoucherState.Status.Selected)
                     val newData = listOf(
                         newDataTest, ItemVoucherState(
                             id = "1",
